be76f6f3c94db06f665362f5e3cc060306e9a76c
Amanda Solsona <amandasolsona0@gmail.com>
2023-08-03 11:09:24 -0300

    Exercício 02

---
+++ 2_exercicio_02/scripts.js
@@ -0,0 +1,6 @@
+/* Escreva três valores em number em um arquivo e exiba no navegador com o console.log();
+Com números inteiros, números com casa decimal e por aritmética;*/
+
+console.log(235);
+console.log(13.5);
+console.log((3 * (12 + 4)) / 8);
