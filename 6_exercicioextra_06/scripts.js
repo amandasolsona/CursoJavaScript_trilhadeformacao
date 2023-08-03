7d851c0f3211bc5491978ba42a8cb613210026b6
Amanda Solsona <amandasolsona0@gmail.com>
2023-08-03 11:26:54 -0300

    Exercício extra 06

---
+++ 6_exercicioextra_06/scripts.js
@@ -0,0 +1,9 @@
+/*Crie uma variável chamada isActive com o valor booleano de true;
+Imprima com console.log o valor da variável */
+
+/*let é uma palavra-chave em JS usada para declarar variáveis. Ela permite que você crie uma variável
+ com escopo de bloco, o que significa que a variávelsó pode ser acessada dentro do bloco de código
+  em que foi declarada. let # var */
+
+let isActive = true;
+console.log(isActive);
