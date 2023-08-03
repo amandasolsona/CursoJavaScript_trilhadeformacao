2b605dd932bf651bd1175b093b8c53c1a89462b5
Amanda Solsona <amandasolsona0@gmail.com>
2023-08-03 11:05:52 -0300

    Exercício 01

---
+++ 1_exercicio_01/scripts.js
@@ -0,0 +1,6 @@
+/* Escreva três valores em string em um arquivo e exiba no navegador com o console.log(); 
+com aspas duplas, simples e template literals */
+
+console.log("Hello World!");
+console.log('Meu nome é Amanda Solsona');
+console.log(`Este aqui é o template literals`);
