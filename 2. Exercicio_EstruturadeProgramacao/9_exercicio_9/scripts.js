fbfcabafc4affa8529ff5b0317f2d62d7e0e4f73
Amanda Solsona <amandasolsona0@gmail.com>
2023-08-04 09:46:00 -0300

    Exercício 09 - Estrutura de Programação

---
+++ 2. Exercicio_EstruturadeProgramacao/9_exercicio_9/scripts.js
@@ -0,0 +1,12 @@
+/*Escreva um loop for ou while que exiba qual o número é par e qual
+é o número é impar, o contador deve iniciar em 0 e ir até 50;*/
+
+for(let i = 0; i <= 50; i++) {
+  
+  if(i % 2 == 0) {
+    console.log(`O numéro ${i} é par`);
+  } else {
+    console.log(`O numéro ${i} é ímpar`);
+  }
+
+}
