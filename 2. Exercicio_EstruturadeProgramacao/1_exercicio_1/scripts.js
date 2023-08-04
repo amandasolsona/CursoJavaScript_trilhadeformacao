ba9c380914ea54f73de3f26184a4e08b1ceff4d0
Amanda Solsona <amandasolsona0@gmail.com>
2023-08-04 09:11:51 -0300

    Exercício Estrutura de Programação

---
+++ 2. Exercicio_EstruturadeProgramacao/1_exercicio_1/scripts.js
@@ -0,0 +1,10 @@
+/*Armazene em valores em variáveis com cada um dos tipos de dados vistos;
+String, Number e Boolean;*/
+
+let nome = "Amanda";
+let idade = 28;
+let cnh = true;
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof cnh);
