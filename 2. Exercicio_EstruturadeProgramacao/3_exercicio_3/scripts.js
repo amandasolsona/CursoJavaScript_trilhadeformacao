e9a90d45137d6e9c8adc04e92442e6a3e3c6f59e
Amanda Solsona <amandasolsona0@gmail.com>
2023-08-04 09:26:49 -0300

    Exercício 03 - Estrutura de Programação

---
+++ 2. Exercicio_EstruturadeProgramacao/3_exercicio_3/scripts.js
@@ -0,0 +1,9 @@
+/*Crie uma constante com o seu nome como valor; Depois uma estrutura if que verifica
+se o seu nome é o que está na constante; Se estiver, emita uma mensagem de saudação com
+console.log(); */
+
+const nome = "Amanda Solsona";
+
+if(nome == "Amanda Solsona") {
+  console.log(`Olá ${nome}, tudo bem?`);
+}
